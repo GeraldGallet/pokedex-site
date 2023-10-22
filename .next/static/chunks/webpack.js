ff6b3a8801af2af30945710d9a5aa18da2f2f104
GeraldGallet <gerald.gallet.gg@gmail.com>
2023-10-22 14:21:21 +0200

    chore(*): remove next folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7fd28b753139e735"; }
+/******/ 		__webpack_require__.h = function() { return "9fa078c66fc045a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
